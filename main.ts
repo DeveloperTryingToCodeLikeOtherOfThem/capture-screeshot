38100be98b150e67fef6db8eee589c1e6aa4d3db
a012 <Aeagan0507@gmail.com>
2025-11-12 22:24:06 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -10,21 +10,21 @@ namespace ScreenCaptureKind {
     export const CaptureScreen = ScreenCaptureKind.create()
 }
 
- namespace screenCapture {
-  //% block="capture screen image at %x %y scale of captured screen image || %sx %sy"
-  //% blockSetVariable=captureScreenSprite
-  //% x.defl=80
-  //% y.defl=60
- //% sx.defl=1 
-  //% sy.defl=1
-  export function captureScreenImageAt(x: number = 80, y: number = 60, sx = 1, sy = 1): Sprite {
-    let screenCaptureSprite = sprites.create(screen, ScreenCaptureKind.CaptureScreen)
-    screenCaptureSprite.x = x 
-    screenCaptureSprite.y = y
-    screenCaptureSprite.sx = sx 
-    screenCaptureSprite.sy = sy
-    return screenCaptureSprite
-  }
+namespace screenCapture {
+    //% block="capture screen image at %x %y scale of captured screen image || %sx %sy"
+    //% blockSetVariable=captureScreenSprite
+    //% x.defl=80
+    //% y.defl=60
+    //% sx.defl=1 
+    //% sy.defl=1
+    export function captureScreenImageAt(x: number = 80, y: number = 60, sx = 1, sy = 1): Sprite {
+        let screenCaptureSprite = sprites.create(screen, ScreenCaptureKind.CaptureScreen)
+        screenCaptureSprite.x = x
+        screenCaptureSprite.y = y
+        screenCaptureSprite.sx = sx
+        screenCaptureSprite.sy = sy
+        return screenCaptureSprite
+    }
 
-  
- }
+
+}
