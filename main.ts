c03f3225e201a78741194841e120b03a4cc859c4
a012 <Aeagan0507@gmail.com>
2025-11-12 23:53:30 -0500

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -20,7 +20,7 @@ namespace screenCapture {
     //% sy.defl=scene.screenHeight()
     export function captureScreenImageAt(x: number, y: number, sx = 2, sy = 2): Sprite {
         let screenCaptureSprite = sprites.create(image.create(screen.width, screen.height), ScreenCaptureKind.CaptureScreen)
-        screenCaptureSprite.image.blit(x, y, sx, sy, screen, screenCaptureSprite.x, screenCaptureSprite.y, sx, sy, true, false)
+        screenCaptureSprite.image.blit(x, y, sx, sy, screen, screenCaptureSprite.x, screenCaptureSprite.y, sx / screen.width, sy / screen.height, true, false)
         return screenCaptureSprite
     }
 
