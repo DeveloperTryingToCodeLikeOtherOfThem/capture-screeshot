14f03293b69c743fc63e39f9e139fc8dc35659df
a012 <Aeagan0507@gmail.com>
2025-11-12 22:21:42 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -13,11 +13,11 @@ namespace ScreenCaptureKind {
  namespace screenCapture {
   //% block="capture screen image at %x %y scale of captured screen image || %sx %sy"
   //% blockSetVariable=captureScreenSprite
-  //% x.defl=scene.screenWidth() >> 1
-  //% y.defl=scene.screenHeight() >> 1
+  //% x.defl=80
+  //% y.defl=60
  //% sx.defl=1 
   //% sy.defl=1
-  export function captureScreenImageAt(x: number = scene.screenWidth(), y: number = scene.screenHeight(), sx = scene.screenWidth(), sy = scene.screenHeight()): Sprite {
+  export function captureScreenImageAt(x: number = 80, y: number = 60, sx = 1, sy = 1): Sprite {
     let screenCaptureSprite = sprites.create(screen, ScreenCaptureKind.CaptureScreen)
     screenCaptureSprite.x = x 
     screenCaptureSprite.y = y
